c180d5d56198bd96b4112f8a9ca0bb469e987093
choiwa <joyjoycho@gmail.com>
2018-06-18 18:27:38 -0700

    Dancer class generates a blinking dancer class that passes all tests.

--- src/dancer.js
+++ src/dancer.js
@@ -12,19 +12,19 @@ var makeDancer = function(top, left, timeBetweenSteps) {
 
 };
 
-  makeDancer.prototype.step = function() {
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
-    setTimeout(this.step.bind(this), this.timeBetweenSteps);
-  };
+makeDancer.prototype.step = function() {
+  // the basic dancer doesn't do anything interesting at all on each step,
+  // it just schedules the next step
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
+};
 
-  makeDancer.prototype.setPosition = function(top, left) {
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
-    //
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    this.$node.css(styleSettings);
+makeDancer.prototype.setPosition = function(top, left) {
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  //
+  var styleSettings = {
+    top: top,
+    left: left
   };
+  this.$node.css(styleSettings);
+};

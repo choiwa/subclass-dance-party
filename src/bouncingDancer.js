e6c507258f6f21ca4795677640f2400f1ccb64b8
choiwa <joyjoycho@gmail.com>
2018-06-19 14:19:31 -0700

    No changes to code.

--- src/bouncingDancer.js
+++ src/bouncingDancer.js
@@ -13,17 +13,17 @@ makeBouncingDancer.prototype.constructor = makeBouncingDancer;
 makeBouncingDancer.prototype.step = function() {
 // console.log('hello')
   makeDancer.prototype.step.call(this);
-  if(this.move === true) {
-  //left = left + velX;
-  this.left += this.velX;
-  //console.log($(window).width());
-  if (this.left >= $(window).width() - 50 ) {
-    this.left = $(window).width() - 50;
-    this.velX = -this.velX;
-  } else if (this.left <= 0) {
-    this.velX = -this.velX;
-    this.left = 10;
+  if (this.move === true) {
+    //left = left + velX;
+    this.left += this.velX;
+    //console.log($(window).width());
+    if (this.left >= $(window).width() - 50 ) {
+      this.left = $(window).width() - 50;
+      this.velX = -this.velX;
+    } else if (this.left <= 0) {
+      this.velX = -this.velX;
+      this.left = 10;
+    }
+    this.setPosition(this.top, this.left);
   }
-  this.setPosition(this.top, this.left);
-}
 };

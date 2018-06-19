59afda1c021aa4552cf7c57b5cb5e2bf3849cdb3
choiwa <joyjoycho@gmail.com>
2018-06-19 12:36:45 -0700

    Add a lineUp() method.

--- src/dancer.js
+++ src/dancer.js
@@ -9,6 +9,7 @@ var makeDancer = function(top, left, timeBetweenSteps) {
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
   this.setPosition(top, left);
+  this.left = left;
 
 };
 
@@ -27,4 +28,10 @@ makeDancer.prototype.setPosition = function(top, left) {
     left: left
   };
   this.$node.css(styleSettings);
+
+
 };
+
+makeDancer.prototype.lineUp = function() {
+  this.left = 10;
+}

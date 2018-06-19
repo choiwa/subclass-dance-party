6bdc0ba1ddba3a50191b66d689d973f6d591c7c1
choiwa <joyjoycho@gmail.com>
2018-06-18 21:08:04 -0700

    Blinking dancer updated to blink correctly at random locations.

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -4,7 +4,7 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
-//  var oldStep = this.step;
+  //  var oldStep = this.step;
   //this.step(100);
 
 
@@ -14,12 +14,12 @@ makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
 makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 makeBlinkyDancer.prototype.step = function() {
   makeDancer.prototype.step.call(this);
-    // call the old version of step at the beginning of any call to this new version of step
-    // oldStep();
+  // call the old version of step at the beginning of any call to this new version of step
+  // oldStep();
     
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
   this.$node.toggle();
-  };
+};
 

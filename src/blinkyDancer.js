f3682d63cd0dcc7f4db2d475818a90bca0de7ed6
choiwa <joyjoycho@gmail.com>
2018-06-18 18:24:53 -0700

    blinkyDancer blinks and passes all tests.

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -12,15 +12,14 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
 makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
-
-  makeBlinkyDancer.prototype.step = function() {
-   makeDancer.prototype.step.call(this);
-    // call the old version of step at the beginning of any call to this new version of step
-    // oldStep();
+makeBlinkyDancer.prototype.step = function() {
+  makeDancer.prototype.step.call(this);
+  // call the old version of step at the beginning of any call to this new version of step
+  // oldStep();
     
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    this.$node.toggle();
-  };
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.toggle();
+};
 
